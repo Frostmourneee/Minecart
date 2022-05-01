be2e550bd6cf7042c8713f753e4ae9c3dc0d843f
Frostmourneee <dolpad@yandex.ru>
2022-05-01 14:39:46 +0300

    test

--- src/main/java/com/frostmourneee/minecart/common/entity/AbstractCart.java
+++ src/main/java/com/frostmourneee/minecart/common/entity/AbstractCart.java
@@ -69,7 +69,9 @@ public abstract class AbstractCart extends AbstractMinecart {
         delta = position().subtract(xOld, yOld, zOld);
         verticalMovementType.add(goesUp() ? 1 : goesFlat() ? 0 : -1);
         if (verticalMovementType.size() == 3) verticalMovementType.remove(0);
-        if (!zeroDeltaMovement()) customPrint(this, ccUtil.vecToDirection(delta));
+        if (!zeroDeltaMovement()) {
+            //fix
+        }
 
         restoreRelativeCarts();
         posCorrectionToFrontCart();
